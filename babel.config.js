fdff20a3145085cd90f9ff3fc67a4ffc209dbb8e
Carterahart18 <carterahart@gmail.com>
2019-07-19 00:13:57 -0700

    Configure babel, typescript, webpack, eslint

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,9 @@
-const presets = [
-  [
-    '@babel/env',
-    {
-      targets: {
-        edge: '17',
-        firefox: '60',
-        chrome: '67',
-        safari: '11.1'
-      },
-      useBuiltIns: 'usage'
-    }
+const config = {
+  presets: ['@babel/env',  '@babel/react', '@babel/typescript'],
+  plugins: [
+    '@babel/proposal-class-properties',
+    '@babel/proposal-object-rest-spread'
   ]
-];
+};
 
-module.exports = { presets };
+module.exports = config;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,11 +3,24 @@ const path = require('path');
 const BASE_PATH = path.resolve(__dirname, '..');
 
 const config = {
+  devtool: 'inline-source-map',
+  entry: path.resolve(BASE_PATH, './src/index.tsx'),
   mode: 'development',
-  entry: path.resolve(BASE_PATH, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(BASE_PATH, './dist')
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts']
   }
 };
 
